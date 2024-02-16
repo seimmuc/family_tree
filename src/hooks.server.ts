14d518ef0df1bfd859551356a36891a28f1b41bd
sei <29907321+seimmuc@users.noreply.github.com>
2024-02-15 17:27:21 -0800

    create database indexes when server starts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
 import { building } from '$app/environment';
-import { getDbConnection } from '$lib/server/graph/memgraph';
+import { getDbConnection, useSession } from '$lib/server/graph/memgraph';
 
 if (!building) {
   try {
@@ -8,8 +8,18 @@ if (!building) {
     if (!auth_success) {
       console.error('Database authentication failed!');
     }
+    const startQueries = [
+      'CREATE INDEX ON :Person;',
+      'CREATE INDEX ON :Person(id);',
+      'CREATE INDEX ON :Person(firstName);',
+    ]
+    await useSession(async s => {
+      for (const q of startQueries) {
+         await s.run(q);
+      }
+    });
   } catch (error) {
     console.error('Failed to connect to the database');
-    console.error(error);
+    throw error;
   }
 }
