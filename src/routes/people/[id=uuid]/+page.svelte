a864d58342f58b4ab2fe09866fe0595172af25d6
sei <29907321+seimmuc@users.noreply.github.com>
2024-08-05 00:57:19 -0700

    remove old debug code

--- src/routes/people/[id=uuid]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { clearEmptyVals, clearKeys } from '$lib/utils';
-
-  export let data;
-</script>
-
-<h1>{data.person.name}</h1>
-<div>{JSON.stringify(clearKeys(clearEmptyVals(data.person), ['id', 'name', 'lastName']))}</div>
--- src/routes/people/gods/recreate-gods/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let data;
-</script>
-
-<p>re-created gods</p>
-<p>people removed: {data.peopleDeleted}</p>
-<p>people added: {data.peopleAdded}</p>
